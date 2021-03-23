59713d403d7e9b8e2d257cd2e74996207c80c704
Jonathan Archer <jonarcher@hotmail.co.uk>
2021-03-23 21:57:47 +0000

    Revised Flocking Algorithm

---
+++ Assets/Scripts/Flocking/Alignment.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behaviour/Alignment")]
+public class Alignment : FlockBehaviour
+{
+    public override Vector3 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        //If no neighbors, maintain current allignment
+        if(context.Count == 0)
+        {
+            return agent.transform.forward;
+        }
+
+        //Add all points together and average
+        Vector3 alignmentMove = Vector3.zero;
+        float alignmentMoveX = 0f;
+        float alignmentMoveZ = 0f;
+
+        foreach(Transform item in context)
+        {
+            alignmentMoveX += item.transform.transform.forward.x;
+            alignmentMoveZ += item.transform.transform.forward.z;
+        }
+
+        alignmentMove = new Vector3(alignmentMoveX, 0, alignmentMoveZ);
+
+        alignmentMove /= context.Count;
+
+        return alignmentMove;
+    }
+}
---
+++ Assets/Scripts/Flocking/Avoidance.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behaviour/Avoidance")]
+public class Avoidance : FlockBehaviour
+{
+    public override Vector3 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        //If no neighbors, return no adjustment
+        if(context.Count == 0)
+        {
+            return Vector3.zero;
+        }
+
+        //Add all points together and average
+        Vector3 avoidanceMove = Vector3.zero;
+        float avoidanceMoveX = 0f;
+        float avoidanceMoveZ = 0f;
+
+        int nAvoid = 0;
+
+        foreach(Transform item in context)
+        {
+            if(Vector3.SqrMagnitude(item.position - agent.transform.position) < flock.SquareAvoidanceRadius)
+            {
+                nAvoid++;
+                avoidanceMoveX += (agent.transform.position.x - item.position.x);
+                avoidanceMoveZ += (agent.transform.position.z - item.position.z);
+            }
+        }
+
+        avoidanceMove = new Vector3(avoidanceMoveX, 0, avoidanceMoveZ);
+
+        if(nAvoid > 0) {
+            avoidanceMove /= nAvoid;
+        }
+
+        return avoidanceMove;
+    }
+}
